1638732000a3adc20de0dbe1f3d9a1a9d1639a68
mmorera92 <60393645+mmorera92@users.noreply.github.com>
2020-08-17 15:14:03 -0600

    Implementing waits.

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -2,8 +2,14 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class BasePage extends indexPages {
+
+    WebDriverWait wait = new WebDriverWait(getDriver(), 20);
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -24,5 +30,19 @@ public class BasePage extends indexPages {
     public void getText(WebElement element)  {
         element.getText();
     }
+
+    public void waitForElementToLoad(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForPageToLoad(){
+        ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        wait.until(pageLoadCondition);
+    }
+
 
 }
