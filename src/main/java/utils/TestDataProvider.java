657adb2c01449dd3324b8bd35035952fb9d91a8e
mmorera92 <60393645+mmorera92@users.noreply.github.com>
2020-08-17 16:44:39 -0600

    Adding data provider

---
+++ src/main/java/utils/TestDataProvider.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class TestDataProvider {
+    public String store = "Calabasas";
+    public String storeSecondSelected = "Century City";
+}
--- src/test/java/UITests/TestClass.java
+++ src/test/java/UITests/TestClass.java
@@ -1,25 +1,27 @@
 package UITests;
 
 import base.BaseTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.StorePickupModalPage;
 import org.testng.Assert;
+import utils.TestDataProvider;
 
 public class TestClass extends BaseTest {
 
+    TestDataProvider testDataProvider = new TestDataProvider();
+
     @Test
     public void openCatering() throws InterruptedException {
-        String store= "Calabasas";
-        String storeSecondSelected = "Century City";
 
         storePickupModalPage = new StorePickupModalPage(getDriver());
         storePickupModalPage.clickOnSelectAStoreModalButton();
-        storePickupModalPage.clickOnAnyStore(store);
+        storePickupModalPage.clickOnAnyStore(testDataProvider.store);
         storePickupModalPage.clickOnEditLink();
-        storePickupModalPage.clickOnAnyStore(storeSecondSelected);
+        storePickupModalPage.clickOnAnyStore(testDataProvider.storeSecondSelected);
         cateringPage = storePickupModalPage.clickOnNextBtn();
         //Asserts
-        Assert.assertTrue(cateringPage.verifySuperMarketSelected(storeSecondSelected), "The Store Second selected is not displayed");
+        Assert.assertTrue(cateringPage.verifySuperMarketSelected(testDataProvider.storeSecondSelected), "The Store Second selected is not displayed");
         
     }
 }
